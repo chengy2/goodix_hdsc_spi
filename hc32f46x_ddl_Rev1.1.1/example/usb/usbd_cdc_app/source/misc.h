c47b13490e16f14b655f1c2dbff2a1b2466274a0
wangmin83 <48580087+wangmin83@users.noreply.github.com>
2020-10-12 11:32:05 +0800

    A version from goodix_hdsc

---
+++ hc32f46x_ddl_Rev1.1.1/example/usb/usbd_cdc_app/source/misc.h
@@ -0,0 +1,92 @@
+/*****************************************************************************
+ * Copyright (C) 2016, Huada Semiconductor Co.,Ltd All rights reserved.
+ *
+ * This software is owned and published by:
+ * Huada Semiconductor Co.,Ltd ("HDSC").
+ *
+ * BY DOWNLOADING, INSTALLING OR USING THIS SOFTWARE, YOU AGREE TO BE BOUND
+ * BY ALL THE TERMS AND CONDITIONS OF THIS AGREEMENT.
+ *
+ * This software contains source code for use with HDSC
+ * components. This software is licensed by HDSC to be adapted only
+ * for use in systems utilizing HDSC components. HDSC shall not be
+ * responsible for misuse or illegal use of this software for devices not
+ * supported herein. HDSC is providing this software "AS IS" and will
+ * not be responsible for issues arising from incorrect user implementation
+ * of the software.
+ *
+ * Disclaimer:
+ * HDSC MAKES NO WARRANTY, EXPRESS OR IMPLIED, ARISING BY LAW OR OTHERWISE,
+ * REGARDING THE SOFTWARE (INCLUDING ANY ACCOMPANYING WRITTEN MATERIALS),
+ * ITS PERFORMANCE OR SUITABILITY FOR YOUR INTENDED USE, INCLUDING,
+ * WITHOUT LIMITATION, THE IMPLIED WARRANTY OF MERCHANTABILITY, THE IMPLIED
+ * WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE OR USE, AND THE IMPLIED
+ * WARRANTY OF NONINFRINGEMENT.
+ * HDSC SHALL HAVE NO LIABILITY (WHETHER IN CONTRACT, WARRANTY, TORT,
+ * NEGLIGENCE OR OTHERWISE) FOR ANY DAMAGES WHATSOEVER (INCLUDING, WITHOUT
+ * LIMITATION, DAMAGES FOR LOSS OF BUSINESS PROFITS, BUSINESS INTERRUPTION,
+ * LOSS OF BUSINESS INFORMATION, OR OTHER PECUNIARY LOSS) ARISING FROM USE OR
+ * INABILITY TO USE THE SOFTWARE, INCLUDING, WITHOUT LIMITATION, ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES OR LOSS OF DATA,
+ * SAVINGS OR PROFITS,
+ * EVEN IF Disclaimer HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ * YOU ASSUME ALL RESPONSIBILITIES FOR SELECTION OF THE SOFTWARE TO ACHIEVE YOUR
+ * INTENDED RESULTS, AND FOR THE INSTALLATION OF, USE OF, AND RESULTS OBTAINED
+ * FROM, THE SOFTWARE.
+ *
+ * This software may be replicated in part or whole for the licensed use,
+ * with the restriction that this Disclaimer and Copyright notice must be
+ * included with each copy of this software, whether used in part or whole,
+ * at all times.
+ */
+/******************************************************************************/
+/** \file misc.h
+ **
+ ** A detailed description is available at
+ ** @link Header file for misc.c @endlink
+ **
+ ******************************************************************************/
+#ifndef __MISC_H__
+#define __MISC_H__
+
+/*******************************************************************************
+ * Include files
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Global type definitions ('typedef')
+ ******************************************************************************/
+
+
+/* SPI_NSS Port/Pin definition */
+#define SPI_NSS_PORT                    (PortB)
+#define SPI_NSS_PIN                     (Pin09)
+
+#define SPI_NSS_HIGH()                  (PORT_SetBits(SPI_NSS_PORT, SPI_NSS_PIN))
+#define SPI_NSS_LOW()                   (PORT_ResetBits(SPI_NSS_PORT, SPI_NSS_PIN))
+
+/*******************************************************************************
+  Global function prototypes (definition in C source)
+ ******************************************************************************/
+void HAL_NVIC_SetPriorityGrouping(uint32_t PriorityGroup);
+void HAL_NVIC_SetPriority(IRQn_Type IRQn, uint32_t PreemptPriority, uint32_t SubPriority);
+void HAL_NVIC_ClearPendingIRQ(IRQn_Type IRQn);
+void HAL_NVIC_EnableIRQ(IRQn_Type IRQn);
+void HAL_NVIC_DisableIRQ(IRQn_Type IRQn);
+uint8_t SpiFlash_WriteReadByte(uint8_t u8Data);
+en_result_t SpiFlash_ReadData(uint32_t u32Addr, uint8_t pData[], uint16_t len);
+void MasterSpiDmaInit(void);
+en_result_t HAL_SPI_Receive_DMA(const M4_SPI_TypeDef *SPIx, uint8_t pu8Data[], uint16_t len);
+void MasterSpiInit(void);
+void USB_DPDMWakeupConfig(void);
+void USB_DPDM_PortInit(void);
+void EcIntConfig(void);
+void FPIntConfig(void);
+uint32_t GetECStatus(void);
+void StopModeConfig(void);
+
+#endif /*__MISC_H__*/
+
+/*******************************************************************************
+ * EOF (not truncated)
+ ******************************************************************************/
